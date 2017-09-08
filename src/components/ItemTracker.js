e83cded8c2419f7beefa5a58ef766334161ce799
kaisukez <zykigix@hotmail.com>
2017-09-09 01:10:47 +0700

    Updates

--- src/components/ItemTracker.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default (props) => {
-  return (
-    <span id="itemLeft" className="input-group-addon">
-      {props.num} {props.num > 1 ? " items" : " item"} left
-    </span>
-  )
-}
